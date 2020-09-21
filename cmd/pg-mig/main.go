049eb35b0b7d6918c23d38918686ee7a79c31fdc
Djordje Vukovic <djvukovic@outlook.com>
2020-09-21 10:40:29 +0200

    back

--- cmd/pg-mig/main.go
+++ cmd/pg-mig/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 
 	if len(os.Args) < 2 {
-		fmt.Println("Missing command. Please run pg-mig help to see more infoo")
+		fmt.Println("Missing command. Please run pg-mig help to see more info")
 		return
 	}
 
