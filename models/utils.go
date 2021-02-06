87b306fee546305bba606974278b867d4fa4f7c5
Djordje Vukovic <djordjev@Djordjes-iMac-Pro.local>
2021-02-06 22:24:35 +0100

    Add connection timeout

--- models/utils.go
+++ models/utils.go
@@ -3,13 +3,21 @@ package models
 import (
 	"context"
 	"github.com/jackc/pgx/v4"
+	"time"
 )
 
-func BuildConnector(ctx context.Context, str string) (DBConnection, error) {
-	conn, err := pgx.Connect(ctx, str)
-	if err != nil {
-		return nil, err
+const reconnectCount = 3
+const reconnectTimeout = 3
+
+func BuildConnector(ctx context.Context, str string) (conn DBConnection, err error) {
+	for i := 0; i < reconnectCount; i++ {
+		conn, err := pgx.Connect(ctx, str)
+		if err == nil {
+			return conn, err
+		}
+
+		time.Sleep(reconnectTimeout * time.Second)
 	}
 
-	return conn, err
+	return
 }
